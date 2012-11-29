f49ccb4492c4adac0cbfc7436a83b01e3d7ab163
Yuriy Vasiyarov <varyous@gmail.com>
2012-11-29 13:40:36 +0400

    Checker is almost ready

--- magento-perfomance-checker.go
+++ magento-perfomance-checker.go
@@ -62,16 +62,18 @@ func (this *RequestInfo) makeRequest() {
     }()
 }
 
-func readUrls() error {
+func readUrls(inRequestsChanel chan *RequestInfo) error {
     db := mysql.New("tcp", "", "127.0.0.1:3306", "root", "", "magento_butik")
     fmt.Println("Connect to magento DB")
     if err := db.Connect(); err != nil {
         fmt.Println("Can not connect to magento DB")
         return err
     }
+    defer func() { inRequestsChanel <- nil}()
     
-    if rows, queryResult, err := db.Query("SELECT request_path, category_id, product_id FROM core_url_write WHERE options <> 'RP' LIMIT 1000"); err != null {
+    if rows, queryResult, err := db.Query("SELECT request_path, category_id, product_id FROM core_url_write WHERE options <> 'RP' LIMIT 1000"); err != nil {
         fmt.Println("Can not query urls")
+
         return err
     } else {
         requestPath := queryResult.Map("request_path")
@@ -81,17 +83,65 @@ func readUrls() error {
         for _, row := range rows{
             request := NewRequestInfo(row.Str(requestPath))
             if row.Int(productId) != 0 {
-                requestPath.RequestUrlType = UrlTypeProduct
+                request.RequestUrlType = UrlTypeProduct
             } else if row.Int(categoryId) != 0 {
-                requestPath.RequestUrlType = UrlTypeCategory
+                request.RequestUrlType = UrlTypeCategory
             } else {
-                requestPath.RequestUrlType = UrlTypeUnknown
+                request.RequestUrlType = UrlTypeUnknown
             }
+            inRequestsChanel <- request
         }
-        
     }
+
     return nil
 }
+
+func makeRequests(inRequestsChanel chan *RequestInfo, outRequestsChanel chan *RequestInfo, noParallelRoutines int) {
+    routines := make(chan int, noParallelRoutines)
+    defer func() { outRequestsChanel <- nil}()
+
+    for request := <- inRequestsChanel; request != nil; request = <- inRequestsChanel {
+        routines <- 1
+        go func(routines chan int, request *RequestInfo, outRequestsChanel chan *RequestInfo) {
+            request.makeRequest()
+            <-routines
+            outRequestsChanel <- request
+        }(routines, request, outRequestsChanel)
+    }
+}
+
+func calculateStat(outRequestsChanel chan *RequestInfo) {
+    totalTime := new(time.Time);
+    var totalFailed int;
+    var totalSuccess int;
+    var totalHttpErrors int;
+    var totalContentLength int64;
+
+    if request := <- outRequestsChanel; request != nil {
+        if request.IsFailed {
+            totalFailed = totalFailed + 1
+        } else if request.ResponseCode == 200 {
+            totalSuccess = totalSuccess + 1
+        } else {
+            totalHttpErrors = totalHttpErrors + 1
+        }
+        totalContentLength = totalContentLength + request.ContentLength
+        totalTime.Add(request.Duration)
+    } else {
+        fmt.Printf("Transactions: %d hits\n", totalFailed + totalSuccess + totalHttpErrors) 
+        fmt.Printf("Availability: %f %\n", 100 - (totalFailed / (totalFailed + totalSuccess + totalHttpErrors)) * 100)
+        fmt.Printf("Elapsed time: %v secs\n", totalTime) 
+        fmt.Printf("Data transferred: %d bytes\n", totalContentLength)
+//        fmt.Printf("Data transferred: %d bytes\n", totalContentLength)
+ 
+    }
+}
+
 func main() {
-     readUrls();
+     inRequestsChanel := make(chan *RequestInfo)
+     outRequestsChanel := make(chan *RequestInfo)
+
+     go readUrls(inRequestsChanel)
+     go makeRequests(inRequestsChanel, outRequestsChanel, 50)
+     calculateStat(outRequestsChanel)
 }
